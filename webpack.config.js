06b192c2c97309f8263de450e4856df0c803a1d0
Quentin Pigné <quentin.pigne@gmail.com>
2021-03-06 12:37:09 +0100

    splitting dev and build executions with development and production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  mode: 'development',
   entry: './index.js',
-  watch: true,
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
