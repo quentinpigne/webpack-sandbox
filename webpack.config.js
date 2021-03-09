d31dddadd8a9d92aa5a5095ca54939965d13f195
Quentin Pigné <quentin.pigne@gmail.com>
2021-03-09 20:01:51 +0100

    using env argument instead of mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 const path = require('path')
 
-module.exports = {
-  entry: './assets/js/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      }
-    ]
+module.exports = (env, argv) => {
+  return {
+    mode: env.production ? 'production' : 'development',
+    entry: './assets/js/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        }
+      ]
+    }
   }
 }
